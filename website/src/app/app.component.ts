5074f82c0be040fbef635e11a7a2e6e18f3c117b
moehike <59832236+scarlettjclark@users.noreply.github.com>
2024-12-09 15:25:47 +0000

    show on click functionality implemented for coding

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CodingComponent } from './coding/coding.component.js';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CodingComponent, NgIf],
   template: `<header> SCARLETT JAY CLARK <br>
                       (she/her)
              </header>
-             <button> Programmer </button> <br>
-             <button> Mathematician </button> <br>
-             <button> Tabletop Gamer </button> <br>`,
+             <button (click)="toggleCoding()"> <h1> Coding </h1> <app-coding *ngIf="!codingHidden;" /> </button> <br>
+             <button> <h1> Wargames </h1> </button> <br>
+             <button> <h1> Cardgames </h1> </button> <br>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'website';
+  title = "Scarlett's Website";
+  codingHidden: boolean = true;
+
+  toggleCoding() {
+    this.codingHidden = !this.codingHidden;
+  }
 }
